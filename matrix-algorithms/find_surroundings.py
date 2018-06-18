4bc4db353d5cbcf5f2478dc5208c6d0541cc86fe
Paulo Comasetto <paulogscomasetto@gmail.com>
2018-06-18 00:58:02 -0300

    Update find_surroundings.py

--- matrix-algorithms/find_surroundings.py
+++ matrix-algorithms/find_surroundings.py
@@ -1,4 +1,4 @@
-def findSurroundings(mat, p, order=1):
+def findSurroundings(mat, p, order=1, e=None):
     """
     Find the surrounding positions of a certain position in the matrix.
     The surroundings have order 1 by default.
@@ -9,7 +9,7 @@ def findSurroundings(mat, p, order=1):
 
     if order <= 0: return [p]
 
-    mat2 = expandMatrix(mat2, order)
+    mat2 = expandMatrix(mat2, order, e)
 
     #goes through the matrix and find the surroundings
     surr = []
@@ -20,19 +20,19 @@ def findSurroundings(mat, p, order=1):
     start4 = (start3[0], start3[1] - order*2)
 
     for j in range(start1[1], start2[1]):
-        if mat2[start1[0]][j] is not None:
+        if mat2[start1[0]][j] is not e:
             surr.append([start1[0], j])
 
     for i in range(start2[0], start3[0]):
-        if mat2[i][start2[1]] is not None:
+        if mat2[i][start2[1]] is not e:
             surr.append([i, start2[1]])
 
     for j in range(start3[1], start4[1], -1):
-        if mat2[start3[0]][j] is not None:
+        if mat2[start3[0]][j] is not e:
             surr.append([start3[0], j])
 
     for i in range(start4[0], start1[0], -1):
-        if mat2[i][start4[1]] is not None:
+        if mat2[i][start4[1]] is not e:
             surr.append([i, start4[1]])
 
     #convert positions so that they are
