37a56c8f6d3b0e5a418dd8dfbef941aa8d49dffa
JeanCHilger <hilgerjeancarlo@gmail.com>
2018-08-29 01:45:33 -0300

    Knapsack algorithm. (There is a better version of the code)

---
+++ dynamic-programming/knapsack-0-1.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+vector<vector<int> > cache;
+int tw; // total weight of the knapsack
+vector<int> values; // vector with the values of the items;
+vector<int> weights; // vector with the weights of the items
+int n; // number of items;
+
+int solve (int i, int w) { // i == item (peso, valor); w == soma total (0 - tw);
+    /*
+    Returns the biggest value that fits in a knapsack weight limit.
+    */
+    if (i == 0 || w == 0) return 0;
+
+    if (cache[i][w] != -999) return cache[i][w];
+
+    if (weights[i] > w) {
+        return cache[i][w] = solve(i - 1, w);
+
+    } else {
+        int a = values[i] + solve(i - 1, w - weights[i]); // put current item
+        int b = solve(i - 1, w); // dont put current item
+
+        return cache[i][w] = max(a, b);
+
+    }
+
+}
+
+int main () {
+
+    cout << "Capacidade da Mochila: ";
+    cin >> tw;
+
+    cout << "Número de itens: ";
+    cin >> n;
+
+    cache.assign(n, vector<int> (tw+1, -999));
+
+    int val, wei; // temporary variables;
+
+    cout << "Descrição dos itens (peso valor)\n";
+    for (int i = 0; i < n; i++) {
+        cin >> wei >> val;
+        weights.push_back(wei);
+        values.push_back(val);
+        cache[i][0] = 0;
+    }
+
+    for (int w = 0; w < tw; w++) {
+        cache[0][w] = values[0];
+    }
+    int g = solve(n-1, tw);
+
+    cout << cache[n-1][tw] << "\n";
+
+    return 0;
+}
