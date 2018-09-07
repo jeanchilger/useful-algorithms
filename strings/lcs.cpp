1794a35172abf109230ce043f53a7c22442af080
JeanCHilger <hilgerjeancarlo@gmail.com>
2018-09-07 16:10:58 -0300

    lcs updated

--- strings/lcs.cpp
+++ strings/lcs.cpp
@@ -7,34 +7,25 @@ using namespace std;
 vector<vector<int> > cache;
 string a, b;
 
-int lcs(int pa, int pb){
+int lcs (int pa, int pb) {
     /*
-    Returns the Longest Common String between two strings.
-    The arguments pa and pb must be the first position of both a and b (0, usually).
-    The algorithm checks the occurrence of characters in the same order but not necessarily
-    in sequence.
+     Returns the Longest Common String between two strings.
+     The arguments pa and pb must be the first position of both a and b (0, usually).
+     The algorithm checks the occurrence of characters in the same order but not necessarily
+     in sequence.
     */
-
-    if (cache[pa][pb] != -1) {
-        return cache[pa][pb];
+    if (pa >= (int)a.length() || pb >= (int)b.length()) {
+        return 0;              
     }
-
-    if (pa >= (int)a.size()){
-        return 0;
-    }
-
-    if (pb >= (int)b.size()){
-        return 0;
-    }
-
-    if (a[pa] == b[pb]){
-        return cache[pa][pb] = 1 + lcs(pa+1, pb+1);
-    }
-
-    int x = cache[pa][pb] = lcs(pa+1, pb);
-    int y = cache[pa][pb] = lcs(pa, pb+1);
-
-    return max(x, y);
+     
+    if (cache[pa][pb] != -1) return cache[pa][pb];
+          
+    if (a[pa] == b[pb]) return cache[pa][pb] = lcs(pa+1, pb+1) + 1;
+               
+    int x = cache[pa][pb] = lcs(pa, pb+1);
+    int y = cache[pa][pb] = lcs(pa+1, pb);
+                         
+    return max(x, y);                          
 }
 
 
