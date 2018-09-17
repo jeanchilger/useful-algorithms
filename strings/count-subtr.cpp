145154acc95116032f203c385c46a9c85f36fe5e
Paulo Comasetto <paulogscomasetto@gmail.com>
2018-09-16 22:44:03 -0300

    Changed "end" default value

--- strings/count-subtr.cpp
+++ strings/count-subtr.cpp
@@ -7,9 +7,9 @@ using namespace std;
 /*counts occurrences of a substring in a larger string
 with overlapping (e.g.: there are 2 occurrences of "aa" in "aaa")*/
 
-int count(string subs, string str, int start=0, int end=0) {
+int count(string subs, string str, int start=0, int end=-1) {
     int occur = 0;
-    end = (end == 0 ? str.length() - subs.length() : end);
+    end = (end == -1 ? str.length() - subs.length() : end);
 
     for (; start < end; start++) {
         string token = str.substr(start, subs.length());
