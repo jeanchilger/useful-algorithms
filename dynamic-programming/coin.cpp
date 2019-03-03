257c12ee2a39a6622c05613d705fd8da2507648b
JeanCHilger <hilgerjeancarlo@gmail.com>
2019-03-02 23:42:52 -0300

    copy number 666.

--- dynamic-programming/coin.cpp
+++ dynamic-programming/coin.cpp
@@ -9,18 +9,41 @@ vector<int> coins;
 
 int v, m; // value to change | number of coins
 
-int coin(int s, int i) { // value left to change, index
+int coin(int s, int i) {
+    /*
+     * Returns whether or not is possible to change the value
+     * v with the given m coins.
+     * There are one coin of each given value.
+     * */
 
     if (s == 0) return 1;
     if (s < 0) return 0;
     if (i == m) return 0;
     if (cache[s][i] != -1) return cache[s][i];
 
-    int a = coin(s - coins[i], i + 1);
+    int a = coin(s - coins[i], i - 1);
     int b = coin(s, i + 1);
 
 
     return cache[s][i] = max(a, b);
+}
+
+int h_m_coin(int s, int i) {
+    /*
+     * Returns how many ways can we change the value v
+     * with the given m coins.
+     * There are infinite coins of each given value.
+     * */
+
+    if (s == 0) return 1;
+    if (s < 0) return 0;
+    if (i == m) return 0;
+    if (cache[s][i] != -1) return cache[s][i];
+
+    int a = coin(s - coins[i], i);
+    int b = coin(s, i + 1);
+
+    return cache[s][i] = a + b;
 }
 
 int main () {
