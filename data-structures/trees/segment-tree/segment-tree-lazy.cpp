015684f503e9d05bd1e6d3d749252dcb49e4f43b
aluno-uffs <aluno-uffs@github.com>
2019-04-03 19:34:47 -0300

    asdsdsa

--- data-structures/trees/segment-tree/segment-tree-lazy.cpp
+++ data-structures/trees/segment-tree/segment-tree-lazy.cpp
@@ -16,6 +16,7 @@ typedef tuple<int, int, int> iii;
 typedef vector <iii> viii;
 
 viii st;
+vector<int> lazy;
 // st[i] -> (val, b, e)
 // where val is the min value between the interval [b, e]
 
@@ -40,7 +41,7 @@ int merge(int a, int b) {
      * Combines the two given values of the tree.
      * */
 
-    return max(a, b);
+    return min(a, b);
 }
 
 void print(int n) {
@@ -50,7 +51,9 @@ void print(int n) {
      * */
 
     for (int i = 0; i < n; i++) {
-        printf("[%d, %d, %d]\n", get<0>(st[i+1]), get<1>(st[i+1]), get<2>(st[i+1]));
+        //printf("[%d, %d, %d]\n", get<0>(st[i+1]), get<1>(st[i+1]), get<2>(st[i+1]));
+        printf("[%d, %d, %d]\n", (lazy[i+1]), get<1>(st[i+1]), get<2>(st[i+1]));
+ 
     }
 
     return;
@@ -85,16 +88,18 @@ int search(int b, int e, int id = 1) {
     int j = get<2>(st[id]);
 
     if ((b == i && j == e) || (b <= i && e >= j)) {
-        return get<0>(st[id]);
+        return get<0>(st[id]) + lazy[id];
     }
 
     if (j < b || i > e) {
-        return -INF;
+        return INF;
     }
 
     if (i <= b || j >= e) {
         return merge(search(b, e, 2 * id), search(b, e, 2 * id + 1));  
     }
+
+    return 0;
 }
 
 void update(int idx, int newVal, int arr[], int id=1) {
@@ -129,6 +134,35 @@ void update(int idx, int newVal, int arr[], int id=1) {
     st[id] = m;
 }
 
+void updateNode(int i, int sum) {
+    /*
+     * Utility function to update a single node.
+     * */
+
+    iii n(sum + get<0>(st[i]), get<1>(st[i]), get<2>(st[i]));
+    st[i] = n;
+    lazy[i] = 0;
+
+}
+
+void updateRange(int l, int r, int sum, int arr[], int id=1) {
+    /*
+     * Updates the array in the given position.
+     * The intervals of the tree are updated as well.
+     * */
+
+    int i = get<1>(st[id]);
+    int j = get<2>(st[id]);
+
+    if (lazy[id] != 0) {
+        updateNode(id, sum);
+    }
+
+
+
+}
+
+
 int main(){
     int n, e, b, o;
 
@@ -141,6 +175,7 @@ int main(){
 
     iii f (0, 0, 0);
     st.assign(2*n, f);
+    lazy.assign(2*n, 0);
     
     init(1, n, 1, arr);
     while (scanf(" %d", &o)!= EOF) {
@@ -149,7 +184,7 @@ int main(){
             int val = search(b, e);
             printf("search(%d, %d) = %d\n", b, e, val);
         } else {
-            update(b, e, arr);
+            updateRange(b, e, 5,  arr);
         }
     }
 
